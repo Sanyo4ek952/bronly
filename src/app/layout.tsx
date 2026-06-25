a4562a99630119b7b2f875ea471c39a7bebbd057
Sanyo4ek958 <sanyo4ek952@gmail.com>
2026-06-26 01:00:39 +0300

    add tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,7 +66,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
-      <body className={manrope.variable}>
+      <body className={`${manrope.variable} antialiased`}>
         <Suspense fallback={null}>
           <TopLoadingBar />
         </Suspense>
